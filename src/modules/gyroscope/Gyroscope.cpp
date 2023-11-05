ed7243032d37e39c2174407725f7ce6606802415
AngeliBazzo <51659435+AngeliBazzo@users.noreply.github.com>
2023-11-05 14:48:19 -0300

    giroscopio

--- src/modules/gyroscope/Gyroscope.cpp
+++ src/modules/gyroscope/Gyroscope.cpp
@@ -1,18 +1,45 @@
 #include "Gyroscope.h"
 
+const int Gyroscope::gyroscope_i2c_address = ADC_I2C_ADDR; // already set in lib
 
+bool Gyroscope::error = false;
+Adafruit_MPU6050 Gyroscope::mpu;
+int minAccelY, maxAccelY;
+double currentPitch, previousPitch;
+unsigned long previousTime;
 
-void Gyroscope::init() {
+void Gyroscope::init()
+{
+
+    if (!mpu.begin()) {
+        printDebug("MPU6050 connection failed. Please check your connections.");
+        Gyroscope::error = true;
+        while (1);
+    }
 
 }
 
-angle Gyroscope::acquire() {
-   return 0.0f; 
+void Gyroscope::calibrateMPU6050() {
+
+  for (int i = 0; i < 1000; i++) {
+    sensors_event_t accel, gyro, temp;
+    mpu.getEvent(&accel, &gyro, &temp);
+
+    if (i == 0) {
+      minAccelY = maxAccelY = accel.acceleration.y;
+    } else {
+      if (accel.acceleration.y < minAccelY) minAccelY = accel.acceleration.y;
+      if (accel.acceleration.y > maxAccelY) maxAccelY = accel.acceleration.y;
+    }
+    delay(1);
+  }
 }
 
-angle Gyroscope::getLastValue() {
-    return Gyroscope::last_value;
+double Gyroscope::calculatePitch() {
+  sensors_event_t accel, gyro, temp;
+  mpu.getEvent(&accel, &gyro, &temp);
+  //int pitchValue = map(accel.acceleration.y, minAccelY, maxAccelY, -90, 90);
+  double pitch = RAD_TO_DEG * (atan2(accel.acceleration.x, accel.acceleration.z) + PI);
+  return pitch;
 }
 
-void Gyroscope::sendLastValue() {
-}

ba9004d88d286d5e2afe78b2511c2267c1320426
AngeliBazzo <51659435+AngeliBazzo@users.noreply.github.com>
2023-11-05 14:48:19 -0300

    giroscopio

--- src/modules/gyroscope/Gyroscope.h
+++ src/modules/gyroscope/Gyroscope.h
@@ -1,11 +1,17 @@
 #ifndef GYROSCOPE_MODULE
 #define GYROSCOPE_MODULE
+<<<<<<< Updated upstream
 
 typedef float angle;
+=======
+#include <Adafruit_MPU6050.h>
+#include "../debug/debug.h"
+>>>>>>> Stashed changes
 
 class Gyroscope
 {
 private:
+<<<<<<< Updated upstream
     static float last_value;
 public:
 
@@ -16,6 +22,18 @@ public:
     static angle getLastValue();
     static void sendLastValue();
 
+=======
+    static const int gyroscope_i2c_address; 
+    //static Adafruit_ADS1115 ads;
+    static Adafruit_MPU6050 mpu;
+    static bool error;
+public:
+    Gyroscope() = delete;
+    ~Gyroscope() = delete;
+    static void init();
+    static void calibrateMPU6050();
+    static double calculatePitch();
+>>>>>>> Stashed changes
 };
 
 #endif

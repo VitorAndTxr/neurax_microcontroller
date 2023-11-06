0d873a5be08df025c6eef150aeaeae12fd574361
AngeliBazzo <51659435+AngeliBazzo@users.noreply.github.com>
2023-11-06 08:19:40 -0300

    giroscopio e teste do modulo

--- src/modules/gyroscope/Gyroscope.cpp
+++ src/modules/gyroscope/Gyroscope.cpp
@@ -35,11 +35,70 @@ void Gyroscope::calibrateMPU6050() {
   }
 }
 
-double Gyroscope::calculatePitch() {
+float Gyroscope::calculatePitch() {
   sensors_event_t accel, gyro, temp;
   mpu.getEvent(&accel, &gyro, &temp);
   //int pitchValue = map(accel.acceleration.y, minAccelY, maxAccelY, -90, 90);
-  double pitch = RAD_TO_DEG * (atan2(accel.acceleration.x, accel.acceleration.z) + PI);
+  float pitch = RAD_TO_DEG * (atan2(accel.acceleration.x, accel.acceleration.z) + PI);
   return pitch;
+}
+
+float Gyroscope::gyroscopeRoutine(){
+    
+    int gyroscope_time = 3000; //3 segundos
+    float initial_position, aux_value, movement_result; 
+    unsigned long startTime = millis();
+
+    //calibra
+    Gyroscope::calibrateMPU6050();
+
+    //le o primeiro valor
+    initial_position = Gyroscope::calculatePitch();
+
+    //le o segundo valor e salva a diferença 
+    aux_value = Gyroscope::calculatePitch();
+    if ((initial_position - aux_value) < 0){
+        movement_result = 0;
+    }
+    else{
+        movement_result = initial_position - aux_value;
+    }
+
+    // tempo de duracao da medida
+    while (millis() - startTime < gyroscope_time) {
+        aux_value = Gyroscope::calculatePitch();
+        if ((initial_position - aux_value) > movement_result){
+            movement_result = initial_position - aux_value;
+        }
+        // Add some delay to avoid constant checking, adjust as needed
+        //delay(100); 
+    }
+    return movement_result;
+
+}
+
+void Gyroscope::testGyroscope(){
+    printDebug("testing gyroscope");
+    //calibra
+    Gyroscope::calibrateMPU6050();
+
+    //le o primeiro valor
+    float position;
+    position = Gyroscope::calculatePitch();
+    printDebug("testing gyroscope ending");
+
+
+}
+
+angle Gyroscope::acquire(){
+    return 90;
+}
+
+angle Gyroscope::getLastValue(){
+    return 90;
+}
+
+void Gyroscope::sendLastValue(){
+
 }
 
--- src/modules/gyroscope/Gyroscope.h
+++ src/modules/gyroscope/Gyroscope.h
@@ -1,17 +1,13 @@
 #ifndef GYROSCOPE_MODULE
 #define GYROSCOPE_MODULE
-<<<<<<< Updated upstream
 
 typedef float angle;
-=======
 #include <Adafruit_MPU6050.h>
 #include "../debug/debug.h"
->>>>>>> Stashed changes
 
 class Gyroscope
 {
 private:
-<<<<<<< Updated upstream
     static float last_value;
 public:
 
@@ -22,18 +18,15 @@ public:
     static angle getLastValue();
     static void sendLastValue();
 
-=======
     static const int gyroscope_i2c_address; 
     //static Adafruit_ADS1115 ads;
     static Adafruit_MPU6050 mpu;
     static bool error;
 public:
-    Gyroscope() = delete;
-    ~Gyroscope() = delete;
-    static void init();
     static void calibrateMPU6050();
-    static double calculatePitch();
->>>>>>> Stashed changes
+    static float calculatePitch();
+    static float gyroscopeRoutine(); 
+    static void testGyroscope();
 };
 
 #endif
